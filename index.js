2dd43fb7c106aba87068cf277dfd7da6d0590c67
Oscar Menéndez <ozkxrmcr@gmail.com>
2016-03-23 13:09:59 -0600

    Add JSDoc

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 /**
- * Match
+ * Test a JSON object
  *
- * @param
- * @param
- * @return
+ * @param {String | Number | Boolean | Object | Array} value The value that will be tested.
+ * @param {*} expected A pattern to test against the value provided.
+ * @return {Boolean} The boolean result of the test.
+ * @api public
  */
 module.exports = require('./lib/match');
