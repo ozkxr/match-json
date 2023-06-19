4da5785f7e3f1bb70fb8eaa1318d9fae6faeea7c
oscar menéndez <omenendez7@gmail.com>
2023-06-18 18:48:12 -0600

    Closes #22

--- test/match_spec.js
+++ test/match_spec.js
@@ -25,9 +25,10 @@ it("match:number", (t) => expect(match(3.141592, 3.141592)).to.equal(true));
 
 it("match:boolean_false", (t) => expect(match(false, false)).to.equal(true));
 
-it("match:boolean_true", (t) => expect(match(false, false)).to.equal(true));
+it("match:boolean_true", (t) => expect(match(true, true)).to.equal(true));
 
-it("match:undefined", (t) => expect(match(undefined, undefined)).to.equal(true));
+it("match:undefined", (t) =>
+  expect(match(undefined, undefined)).to.equal(true));
 
 /**
  * RegExp
@@ -47,9 +48,11 @@ it("match:function", (t) =>
     })
   ).to.equal(true));
 
-it("match:function_arrow", (t) => expect(match(18, (x) => x > 5)).to.equal(true));
+it("match:function_arrow", (t) =>
+  expect(match(18, (x) => x > 5)).to.equal(true));
 
-it("match:function_false", (t) => expect(match(18, (x) => x < 5)).to.equal(false));
+it("match:function_false", (t) =>
+  expect(match(18, (x) => x < 5)).to.equal(false));
 
 /**
  * Objects
@@ -63,7 +66,9 @@ it("match:object_different", (t) =>
   expect(match({ name: "pedro" }, { name: "oscar" })).to.equal(false));
 
 it("match:object_more_keys_value", (t) =>
-  expect(match({ name: "oscar", number: 18 }, { name: "oscar" })).to.equal(false));
+  expect(match({ name: "oscar", number: 18 }, { name: "oscar" })).to.equal(
+    false
+  ));
 
 it("match:object_more_keys_expected", (t) =>
   expect(match({ name: "oscar" }, { name: "", number: 18 })).to.equal(false));
@@ -83,12 +88,16 @@ it("match:object_with_regex_and_functions_fails_all", (t) =>
     match({ name: "oscar", age: 23 }, { name: /ped/, age: (x) => x < 18 })
   ).to.equal(false));
 
+it("match:object_same_lenght_different_keys", (t) =>
+  expect(match({ value: {} }, { league: {} })).to.equal(false));
+
 /**
  * Arrays
  */
 it("match:array_empty", (t) => expect(match([], [])).to.equal(true));
 
-it("match:array", (t) => expect(match([1, 2, "hola"], [1, 2, "hola"])).to.equal(true));
+it("match:array", (t) =>
+  expect(match([1, 2, "hola"], [1, 2, "hola"])).to.equal(true));
 
 it("match:array_different", (t) =>
   expect(match([1, false, "hola"], [1, true, "adios"])).to.equal(false));
@@ -99,7 +108,10 @@ it("match:array_left_more_values", (t) =>
 it("match:array_right_more_values", (t) =>
   expect(match([1, 2], [1, 2, "hola"])).to.equal(false));
 
-it("match:array_with_objects_missing", (t) =>
+it("match:array_with_object_with_less_keys", (t) =>
+  expect(match([1, {}], [1, { test: "aaa" }])).to.equal(false));
+
+it("match:array_with_object_with_more_keys", (t) =>
   expect(match([1, { test: "aaa" }], [1, {}])).to.equal(false));
 
 it("match:array_with_objects_different_value", (t) =>
@@ -113,18 +125,19 @@ it("match:object_with_array_same", (t) =>
     match({ a: [333, { test: "aaa" }] }, { a: [333, { test: "aaa" }] })
   ).to.equal(true));
 
-it("match:array_same", (t) => expect(match([333, 222], [333, 222])).to.equal(true));
+it("match:array_same", (t) =>
+  expect(match([333, 222], [333, 222])).to.equal(true));
 
 it("match:array_with_objects_different_key", (t) =>
   expect(match([1, { test: "aaa" }], [1, { test2: "aaa" }])).to.equal(false));
 
 it("match:array_with_objects_missing_string", (t) =>
-  expect(match([1, { test: "aaa", key2: 33 }], [1, { key2: 33 }])).to.equal(
+  expect(match([1, { key2: 33 }], [1, { test: "aaa", key2: 33 }])).to.equal(
     false
   ));
 
 it("match:array_with_objects_missing_int", (t) =>
-  expect(match([1, { test: "aaa", key2: 33 }], [1, { test: "aaa" }])).to.equal(
+  expect(match([1, { test: "aaa" }], [1, { test: "aaa", key2: 33 }])).to.equal(
     false
   ));
 
@@ -134,12 +147,21 @@ it("match:array_with_objects_missing_int", (t) =>
 it("match:map_empty", (t) => expect(match({}, new Map())).to.equal(true));
 
 it("match:map_equal", (t) =>
-  expect(match({ name: "oscar" }, new Map([["name", "oscar"]]))).to.equal(true));
+  expect(match({ name: "oscar" }, new Map([["name", "oscar"]]))).to.equal(
+    true
+  ));
 
 it("match:map_different", (t) =>
-  expect(match({ name: "oscar" }, new Map([["name", "pedro"]]))).to.equal(false));
+  expect(match({ name: "oscar" }, new Map([["name", "pedro"]]))).to.equal(
+    false
+  ));
 
-it("match:map_with_mote_keys", (t) =>
+it("match:map_with_more_keys", (t) =>
+  expect(
+    match({ name: "oscar", number: 18 }, new Map([["name", "oscar"]]))
+  ).to.equal(false));
+
+it("match:map_with_less_keys", (t) =>
   expect(
     match(
       { name: "oscar" },
@@ -148,11 +170,6 @@ it("match:map_with_mote_keys", (t) =>
         ["number", 18],
       ])
     )
-  ).to.equal(false));
-
-it("match:map_with_less_keys", (t) =>
-  expect(
-    match({ name: "", number: 18 }, new Map([["name", "oscar"]]))
   ).to.equal(false));
 
 it("match:map_with_regex_and_functions", (t) =>
@@ -193,10 +210,13 @@ it("match:map_with_regex_and_functions_fails_all", (t) =>
  */
 it("match:set_empty", (t) => expect(match([], new Set())).to.equal(true));
 
-it("match:set", (t) => expect(match([1, 2, "hola"], new Set([1, 2, "hola"]))).to.equal(true));
+it("match:set", (t) =>
+  expect(match([1, 2, "hola"], new Set([1, 2, "hola"]))).to.equal(true));
 
 it("match:set_different", (t) =>
-  expect(match([1, true, "adios"], new Set([1, false, "hola"]))).to.equal(false));
+  expect(match([1, true, "adios"], new Set([1, false, "hola"]))).to.equal(
+    false
+  ));
 
 it("match:set_with_more_values", (t) =>
   expect(match([1, 2], new Set([1, 2, "hola"]))).to.equal(false));
@@ -208,12 +228,20 @@ it("match:set_with_less_values", (t) =>
  * Native JSON types
  */
 it("match:types_number", (t) => expect(match(3.1415, Number)).to.equal(true));
-it("match:types_number_wrong", (t) => expect(match(3.1415, String)).to.equal(false));
-it("match:types_string", (t) => expect(match("a boring string", String)).to.equal(true));
+it("match:types_number_wrong", (t) =>
+  expect(match(3.1415, String)).to.equal(false));
+it("match:types_string", (t) =>
+  expect(match("a boring string", String)).to.equal(true));
 it("match:types_string_wrong", (t) =>
   expect(match("a boring string", Boolean)).to.equal(false));
 it("match:types_boolean", (t) => expect(match(true, Boolean)).to.equal(true));
-it("match:types_boolean_wrong", (t) => expect(match(false, String)).to.equal(false));
+it("match:types_boolean_wrong", (t) =>
+  expect(match(false, String)).to.equal(false));
+it("match:types_object", (t) => expect(match({}, Object)).to.equal(true));
+it("match:types_array", (t) => expect(match([], Array)).to.equal(true));
+it("match:types_object_wrong", (t) =>
+  expect(match([], Object)).to.equal(false));
+it("match:types_array_wrong", (t) => expect(match({}, Array)).to.equal(false));
 
 /**
  * And everything together
--- test/partial_spec.js
+++ test/partial_spec.js
@@ -3,7 +3,7 @@
 const Lab = require("@hapi/lab");
 const partial = require("..").partial;
 
-const { expect } = require('@hapi/code');
+const { expect } = require("@hapi/code");
 const { it } = (exports.lab = Lab.script());
 
 /**
@@ -23,16 +23,19 @@ it("partial:string", (t) =>
 
 it("partial:number", (t) => expect(partial(3.141592, 3.141592)).to.equal(true));
 
-it("partial:boolean_false", (t) => expect(partial(false, false)).to.equal(true));
+it("partial:boolean_false", (t) =>
+  expect(partial(false, false)).to.equal(true));
 
-it("partial:boolean_true", (t) => expect(partial(false, false)).to.equal(true));
+it("partial:boolean_true", (t) => expect(partial(true, true)).to.equal(true));
 
-it("partial:undefined", (t) => expect(partial(undefined, undefined)).to.equal(true));
+it("partial:undefined", (t) =>
+  expect(partial(undefined, undefined)).to.equal(true));
 
 /**
  * RegExp
  */
-it("partial:regexp", (t) => expect(partial("hola k ase?", /k ase/)).to.equal(true));
+it("partial:regexp", (t) =>
+  expect(partial("hola k ase?", /k ase/)).to.equal(true));
 
 it("partial:regexp_no_match", (t) =>
   expect(partial("hola k ase?", /hello world/)).to.equal(false));
@@ -41,13 +44,17 @@ it("partial:regexp_no_match", (t) =>
  * Functions
  */
 it("partial:function", (t) =>
-  expect(partial(18, function (x) {
-    return x > 5;
-  })).to.equal(true));
+  expect(
+    partial(18, function (x) {
+      return x > 5;
+    })
+  ).to.equal(true));
 
-it("partial:function_arrow", (t) => expect(partial(18, (x) => x > 5)).to.equal(true));
+it("partial:function_arrow", (t) =>
+  expect(partial(18, (x) => x > 5)).to.equal(true));
 
-it("partial:function_false", (t) => expect(partial(18, (x) => x < 5)).to.equal(false));
+it("partial:function_false", (t) =>
+  expect(partial(18, (x) => x < 5)).to.equal(false));
 
 /**
  * Objects
@@ -61,10 +68,14 @@ it("partial:object_different", (t) =>
   expect(partial({ name: "pedro" }, { name: "oscar" })).to.equal(false));
 
 it("partial:object_more_keys_value", (t) =>
-  expect(partial({ name: "oscar", number: 18 }, { name: "oscar" })).to.equal(true));
+  expect(partial({ name: "oscar", number: 18 }, { name: "oscar" })).to.equal(
+    true
+  ));
 
 it("partial:object_more_keys_value2", (t) =>
-  expect(partial({ name: "oscar", number: 18 }, { number: 18 })).to.equal(true));
+  expect(partial({ name: "oscar", number: 18 }, { number: 18 })).to.equal(
+    true
+  ));
 
 it("partial:object_more_keys_expected", (t) =>
   expect(partial({ name: "oscar" }, { name: "", number: 18 })).to.equal(false));
@@ -84,58 +95,50 @@ it("partial:object_with_regex_and_functions_fails_all", (t) =>
     partial({ name: "oscar", age: 23 }, { name: /ped/, age: (x) => x < 18 })
   ).to.equal(false));
 
+it("match:object_same_lenght_different_keys", (t) =>
+  expect(partial({ value: {} }, { league: {} })).to.equal(false));
+
 /**
  * Arrays
  */
 it("partial:array_empty", (t) => expect(partial([], [])).to.equal(true));
 
 it("partial:array", (t) =>
-expect(
-  partial([1, 2, "hola"], [1, 2, "hola"])
-).to.equal(true));
+  expect(partial([1, 2, "hola"], [1, 2, "hola"])).to.equal(true));
 
 it("partial:array_different", (t) =>
   expect(partial([1, false, "hola"], [1, true, "adios"])).to.equal(false));
 
 it("partial:array_left_more_values", (t) =>
-  expect(
-    partial([1, 2, "hola"], [1, 2])
-  ).to.equal(false));
+  expect(partial([1, 2, "hola"], [1, 2])).to.equal(false));
 
 it("partial:array_right_more_values", (t) =>
-  expect(
-    partial([1, 2], [1, 2, "hola"])
-  ).to.equal(false));
+  expect(partial([1, 2], [1, 2, "hola"])).to.equal(false));
 
 it("partial:array_with_objects", (t) =>
-  expect(
-    partial([1, {"test": "aaa"}], [1, {"test": "aaa"}])
-  ).to.equal(true));
+  expect(partial([1, { test: "aaa" }], [1, { test: "aaa" }])).to.equal(true));
 
-it("partial:array_with_objects_missing", (t) =>
-  expect(
-    partial([1, {"test": "aaa"}], [1, {}])
-  ).to.equal(true));
+it("partial:array_with_object_with_less_keys", (t) =>
+  expect(partial([1, {}], [1, { test: "aaa" }])).to.equal(false));
+
+it("partial:array_with_object_with_more_keys", (t) =>
+  expect(partial([1, { test: "aaa" }], [1, {}])).to.equal(true));
 
 it("partial:array_with_objects_different_value", (t) =>
-  expect(
-    partial([1, {"test": "aaa"}], [1, {"test": "b"}])
-  ).to.equal(false));
+  expect(partial([1, { test: "aaa" }], [1, { test: "b" }])).to.equal(false));
 
 it("partial:array_with_objects_different_key", (t) =>
-  expect(
-    partial([1, {"test": "aaa"}], [1, {"test2": "aaa"}])
-  ).to.equal(false));
+  expect(partial([1, { test: "aaa" }], [1, { test2: "aaa" }])).to.equal(false));
 
 it("partial:array_with_objects_missing_string", (t) =>
-  expect(
-    partial([1, {"test": "aaa", "key2": 33}], [1, {"key2": 33}])
-  ).to.equal(true));
+  expect(partial([1, { key2: 33 }], [1, { test: "aaa", key2: 33 }])).to.equal(
+    false
+  ));
 
 it("partial:array_with_objects_missing_int", (t) =>
   expect(
-    partial([1, {"test": "aaa", "key2": 33}], [1, {"test": "aaa"}])
-  ).to.equal(true));
+    partial([1, { test: "aaa" }], [1, { test: "aaa", key2: 33 }])
+  ).to.equal(false));
 
 /**
  * Maps
@@ -143,61 +146,76 @@ it("partial:array_with_objects_missing_int", (t) =>
 it("partial:map_empty", (t) => expect(partial({}, new Map())).to.equal(true));
 
 it("partial:map_equal", (t) =>
-  expect(partial({ name: "oscar" }, new Map([["name", "oscar"]]))).to.equal(true));
+  expect(partial({ name: "oscar" }, new Map([["name", "oscar"]]))).to.equal(
+    true
+  ));
 
 it("partial:map_different", (t) =>
-  expect(partial({ name: "oscar" }, new Map([["name", "pedro"]]))).to.equal(false));
+  expect(partial({ name: "oscar" }, new Map([["name", "pedro"]]))).to.equal(
+    false
+  ));
 
-it("partial:map_with_mote_keys", (t) =>
-  expect(partial(
-    { name: "oscar" },
-    new Map([
-      ["name", "oscar"],
-      ["number", 18],
-    ])
-  )).to.equal(false));
-
-it("partial:map_with_less_keys", (t) =>
+it("partial:map_with_more_keys", (t) =>
   expect(
     partial({ name: "oscar", number: 18 }, new Map([["name", "oscar"]]))
   ).to.equal(true));
 
+it("partial:map_with_less_keys", (t) =>
+  expect(
+    partial(
+      { name: "oscar" },
+      new Map([
+        ["name", "oscar"],
+        ["number", 18],
+      ])
+    )
+  ).to.equal(false));
+
 it("partial:map_with_regex_and_functions", (t) =>
-  expect(partial(
-    { name: "oscar", age: 23 },
-    new Map([
-      ["name", /os/],
-      ["age", (x) => x > 18],
-    ])
-  )).to.equal(true));
+  expect(
+    partial(
+      { name: "oscar", age: 23 },
+      new Map([
+        ["name", /os/],
+        ["age", (x) => x > 18],
+      ])
+    )
+  ).to.equal(true));
 
 it("partial:map_with_regex_and_functions_fails", (t) =>
-  expect(partial(
-    { name: "oscar", age: 23 },
-    new Map([
-      ["name", /ped/],
-      ["age", (x) => x > 18],
-    ])
-  )).to.equal(false));
+  expect(
+    partial(
+      { name: "oscar", age: 23 },
+      new Map([
+        ["name", /ped/],
+        ["age", (x) => x > 18],
+      ])
+    )
+  ).to.equal(false));
 
 it("partial:map_with_regex_and_functions_fails_all", (t) =>
-  expect(partial(
-    { name: "oscar", age: 23 },
-    new Map([
-      ["name", /ped/],
-      ["age", (x) => x < 18],
-    ])
-  )).to.equal(false));
+  expect(
+    partial(
+      { name: "oscar", age: 23 },
+      new Map([
+        ["name", /ped/],
+        ["age", (x) => x < 18],
+      ])
+    )
+  ).to.equal(false));
 
 /**
  * Sets
  */
 it("partial:set_empty", (t) => expect(partial([], new Set())).to.equal(true));
 
-it("partial:set", (t) => expect(partial([1, 2, "hola"], new Set([1, 2, "hola"]))).to.equal(true));
+it("partial:set", (t) =>
+  expect(partial([1, 2, "hola"], new Set([1, 2, "hola"]))).to.equal(true));
 
 it("partial:set_different", (t) =>
-  expect(partial([1, true, "adios"], new Set([1, false, "hola"]))).to.equal(false));
+  expect(partial([1, true, "adios"], new Set([1, false, "hola"]))).to.equal(
+    false
+  ));
 
 it("partial:set_with_more_values", (t) =>
   expect(partial([1, 2], new Set([1, 2, "hola"]))).to.equal(false));
@@ -208,12 +226,24 @@ it("partial:set_with_less_values", (t) =>
 /**
  * Native JSON types
  */
-it("partial:types_number", (t) => expect(partial(3.1415, Number)).to.equal(true));
-it("partial:types_number_wrong", (t) => expect(partial(3.1415, String)).to.equal(false));
-it("partial:types_string", (t) => expect(partial("a boring string", String)).to.equal(true));
-it("partial:types_string_wrong", (t) => expect(partial("a boring string", Boolean)).to.equal(false));
-it("partial:types_boolean", (t) => expect(partial(true, Boolean)).to.equal(true));
-it("partial:types_boolean_wrong", (t) => expect(partial(false, String)).to.equal(false));
+it("partial:types_number", (t) =>
+  expect(partial(3.1415, Number)).to.equal(true));
+it("partial:types_number_wrong", (t) =>
+  expect(partial(3.1415, String)).to.equal(false));
+it("partial:types_string", (t) =>
+  expect(partial("a boring string", String)).to.equal(true));
+it("partial:types_string_wrong", (t) =>
+  expect(partial("a boring string", Boolean)).to.equal(false));
+it("partial:types_boolean", (t) =>
+  expect(partial(true, Boolean)).to.equal(true));
+it("partial:types_boolean_wrong", (t) =>
+  expect(partial(false, String)).to.equal(false));
+it("partial:types_object", (t) => expect(partial({}, Object)).to.equal(true));
+it("partial:types_array", (t) => expect(partial([], Array)).to.equal(true));
+it("partial:types_object_wrong", (t) =>
+  expect(partial([], Object)).to.equal(false));
+it("partial:types_array_wrong", (t) =>
+  expect(partial({}, Array)).to.equal(false));
 
 /**
  * And everything together
@@ -221,18 +251,18 @@ it("partial:types_boolean_wrong", (t) => expect(partial(false, String)).to.equal
 it("partial:everything_together", (t) =>
   expect(
     partial(
-    {
-      name: { first: "Walter", last: "White" },
-      age: 51,
-      breakingBad: true,
-    },
-    {
-      name: { first: /[\w]*/, last: "White" },
-      age: (age) => age > 18,
-      breakingBad: Boolean,
-    }
-  )
-).to.equal(true));
+      {
+        name: { first: "Walter", last: "White" },
+        age: 51,
+        breakingBad: true,
+      },
+      {
+        name: { first: /[\w]*/, last: "White" },
+        age: (age) => age > 18,
+        breakingBad: Boolean,
+      }
+    )
+  ).to.equal(true));
 
 /**
  * Bake function
